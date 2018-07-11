5470a9a304770fc0ab2927a538b902274e7a01e7
colinhogan <colin.hogan.17@tfacorps.org>
2018-07-11 00:20:19 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -7,9 +7,9 @@ rainbow_colors = [
 def change_rainbow_colors(rainbow_colors)
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
   rainbow_colors= []
-  rainbow_colors[0]="red" 
-  rainbow_colors[1]="light_red"
-  rainbow_colors[2]="light_yellow"
+  rainbow_colors[0]=red 
+  rainbow_colors[1]=light_red
+  rainbow_colors[2]=light_yellow
   
 end
 
