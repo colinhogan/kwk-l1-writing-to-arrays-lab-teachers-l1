5cc416e0ba1f6a58b022de4a89c69f97b946ce04
colinhogan <colin.hogan.17@tfacorps.org>
2018-07-11 00:27:49 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,6 +1,6 @@
 
 
-def change_rainbow_colors(rainbow_colors)
+def change_rainbow_colors
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
   rainbow_colors = [
   "yellow",
@@ -13,7 +13,7 @@ def change_rainbow_colors(rainbow_colors)
   return rainbow_colors
 end
 
-def add_colors(rainbow_colors)
+def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
 
   rainbow_colors = ["red", "light_red", "light_yellow"]
